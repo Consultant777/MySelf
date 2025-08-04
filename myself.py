afccc5ce046756be2d19328c7b491ed79f059296
REJWAN <76392817+Consultant777@users.noreply.github.com>
2025-08-04 21:49:52 +0600

    Add files via upload

---
+++ myself.py
@@ -0,0 +1,12 @@
+print("="*80)
+print("Hi, I'm Neamot Ali Rejwan 👋".center(80))
+print("="*80)
+
+print("\n")
+
+profile = """
+💻 Computer Technology Student | 🤖 Python & AI Enthusiast
+📍 Dhaka, Bangladesh | 🌐 Passionate about Learning & Building
+"""
+
+print(profile.center(80))
